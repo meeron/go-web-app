fb57886e8de7f245b82e2de66da2c9ba41dc028c
Miron Jakubowski <mijakubowski@gmail.com>
2022-11-30 20:20:08 +0100

    Clean get product by id tests

--- features/products/routes_test.go
+++ features/products/routes_test.go
@@ -1,11 +1,14 @@
 package products
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"regexp"
 	"testing"
 	"web-app/database"
+	"web-app/web"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/gin-gonic/gin"
@@ -16,34 +19,55 @@ const (
 	getQuery = `SELECT * FROM "products" WHERE "products"."id" = $1 AND "products"."deleted_at" IS NULL ORDER BY "products"."id" LIMIT 1`
 )
 
-func TestGet(t *testing.T) {
+func TestGetProductById(t *testing.T) {
 	dbMock, _ := database.OpenMock()
 
 	r := SetUpRouter()
 	r.GET("/:id", get)
 
-	t.Run("should response with 422 if no product found", func(t *testing.T) {
+	t.Run("should response with 422 when no product found", func(t *testing.T) {
+		// Arrange
 		dbMock.ExpectQuery(regexp.QuoteMeta(getQuery)).
 			WithArgs(123).
 			WillReturnRows(sqlmock.NewRows(nil))
+		resErr := web.Error{}
 
+		// Act
 		req, _ := http.NewRequest("GET", "/123", nil)
 		res := httptest.NewRecorder()
 		r.ServeHTTP(res, req)
+		json.Unmarshal(res.Body.Bytes(), &resErr)
 
+		// Assert
 		assert.Equal(t, http.StatusUnprocessableEntity, res.Code)
+		assert.Equal(t, "NotFound", resErr.ErrorCode)
 	})
 
-	t.Run("should response 200", func(t *testing.T) {
-		dbMock.ExpectQuery(regexp.QuoteMeta(getQuery)).
-			WithArgs(666).
-			WillReturnRows(sqlmock.NewRows(nil))
+	t.Run("should response 200 when product is found", func(t *testing.T) {
+		// Arrange
+		const id uint = 666
+		const name = "Test product"
+		const price float32 = 9.99
 
-		req, _ := http.NewRequest("GET", "/666", nil)
+		expectedRow := sqlmock.NewRows([]string{"id", "name", "price"}).
+			AddRow(id, name, price)
+
+		dbMock.ExpectQuery(regexp.QuoteMeta(getQuery)).
+			WithArgs(id).
+			WillReturnRows(expectedRow)
+		product := Product{}
+
+		// Act
+		req, _ := http.NewRequest("GET", fmt.Sprintf("/%d", id), nil)
 		res := httptest.NewRecorder()
 		r.ServeHTTP(res, req)
+		json.Unmarshal(res.Body.Bytes(), &product)
 
+		// Assert
 		assert.Equal(t, http.StatusOK, res.Code)
+		assert.Equal(t, id, product.Id)
+		assert.Equal(t, name, product.Name)
+		assert.Equal(t, price, product.Price)
 	})
 }
 
